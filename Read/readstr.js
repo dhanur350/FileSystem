712055d8e38c3a402c88cf626903be5ce073bf27
dhanur350 <dhanur297@gmail.com>
2022-03-18 16:30:34 +0530

    Day6

--- Read/readstr.js
+++ Read/readstr.js
@@ -1,5 +1,9 @@
 const fs = require('fs');
 const read = fs.createReadStream('file.txt', 'utf8');
+const write = fs.createWriteStream('file1.txt');
 read.on('data', (chunk) => {
     console.log(chunk);
+})
+read.on('data', (chunk) => {
+    write.write(chunk);
 })
